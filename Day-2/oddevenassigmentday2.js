866006bcd94fc5037c40cbe24fc187a491c70401
ragavid667 <ragavi2307@gmail.com>
2025-01-30 13:50:46 +0100

    oddevenassigmentday2.js

--- Day-2/oddevenassigmentday2.js
+++ Day-2/oddevenassigmentday2.js
@@ -1,18 +1,15 @@
 function isOddOrEven (number){
-
-    let num =13;
-
-    if(num%2===0)
+    if(number%2===0)
     {
-        console.log("The number is divisble by 2 is a even number :" , num)
+        console.log("The number is  even number :" , number)
     }
 
     else
     {
-        console.log("The number is not divisible by 2 is an odd number:", num)
+        console.log("The number is  an odd number:", number)
     }
 
-    return number;
+    
 }
 
-isOddOrEven();
+isOddOrEven(number);
